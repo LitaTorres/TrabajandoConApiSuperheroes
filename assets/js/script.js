1c34b49d1e6d061eb4a11dda8eaa1a2cad2a5097
Leslie Torres <Leslietorresc@gmail.com>
2024-02-26 01:13:12 -0300

    Commit 6 "Footer + estilos"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,51 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
+    //Formulario
+    let formulario = document.getElementById("formulario");
+    formulario.addEventListener("submit", function (event) {
+        event.preventDefault();
+        limpiarErrores();
 
-let formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", function (event) {
-    event.preventDefault();
-    limpiarErrores();
+        let textNumero = document.querySelector(".textNumero").value;
+        let resultado = validar(textNumero);
 
-    let textNumero = document.querySelector(".textNumero").value;
-    let resultado = validar(textNumero);
+        if (resultado == true) {
+            superheroe(textNumero); //antes decía solo funcion exito, pero ahora tb llamamos a la función creada
+            exito();
+        };
+    });
 
-    if (resultado == true) {
-        superheroe(textNumero); //antes decía solo funcion exito, pero ahora tb llamamos a la función creada
-        exito();
-    };
-});
+    function limpiarErrores() {
+        document.querySelector(".resultado").innerHTML = "";
+        document.querySelector(".errorNumero").innerHTML = "";
+    }
 
-function limpiarErrores() {
-    document.querySelector(".resultado").innerHTML = "";
-    document.querySelector(".errorNumero").innerHTML = "";
-}
-
-function exito() {
-    document.querySelector(".resultado").innerHTML = "Super consulta enviada con éxito";
-};
-
-function validar(numero) {
-    let pasamosLaValidacion = true;
-
-    let validacionNumero = /^[0-9]+$/;
-
-    if (validacionNumero.test(numero) == false || numero < 1 || numero > 731) {
-        document.querySelector(".errorNumero").innerHTML = "El número es requerido. Favor ingrese solo número entre el 1 y 731"
-        pasamosLaValidacion = false;
+    function exito() {
+        document.querySelector(".resultado").innerHTML = "Super consulta enviada con éxito";
     };
 
-    return pasamosLaValidacion;
-};
+    function validar(numero) {
+        let pasamosLaValidacion = true;
 
-function superheroe(id) {
+        let validacionNumero = /^[0-9]+$/;
+
+        if (validacionNumero.test(numero) == false || numero < 1 || numero > 731) {
+            document.querySelector(".errorNumero").innerHTML = "El número es requerido. Favor ingrese solo número entre el 1 y 731"
+            pasamosLaValidacion = false;
+        };
+
+        return pasamosLaValidacion;
+    };
+
+    //Conectando con AJAX
+    function superheroe(id) {
         var urlApi = "https://superhero.arielhernandezcl.workers.dev/" + id;
         $.ajax({
-            type: "GET", 
-            url: urlApi, 
-            dataType: "json", 
-            success: function(datosApi) {
+            type: "GET",
+            url: urlApi,
+            dataType: "json",
+            success: function (datosApi) {
+                // recordar que como es un objeto no es necesario usar el forEach (no es array)
                 console.log(datosApi);
                 $('.ordenandoCard').show();
                 $('.card-title').text(`Nombre: ${datosApi.name}`);
@@ -56,11 +58,11 @@ function superheroe(id) {
                 $('.card-text7').text(`Alianzas: ${datosApi.biography.aliases}`);
                 $('.card-img').attr('src', datosApi.image.url);
             },
-            error: function(error) { 
+            error: function (error) {
                 console.log(error)
             }
         });
-
+// Graficando
         $.ajax({
             type: "GET",
             url: urlApi,
@@ -73,25 +75,41 @@ function superheroe(id) {
                         text: `Estadisticas de poder para ${datosApi.name}`
                     },
                     data: [{
-                            type: "pie",
-                            startAngle: 45,
-                            showInLegend: "true",
-                            legendText: "{label}",
-                            indexLabel: "{label} ({y})",
-                            yValueFormatString:"#,##0.#"%"",
-                            dataPoints: [
-                                { label: "Poder", y: `${datosApi.powerstats.power}` },
-                                { label: "Combate", y: `${datosApi.powerstats.combat}` },
-                                { label: "Durabilidad", y: `${datosApi.powerstats.durability}` },
-                                { label: "Rapidez", y: `${datosApi.powerstats.speed}` },
-                                { label: "Fuerza", y: `${datosApi.powerstats.strength}` },
-                                { label: "Inteligencia", y: `${datosApi.powerstats.intelligence}` },
-                            ]
+                        type: "pie",
+                        startAngle: 45,
+                        showInLegend: "true",
+                        legendText: "{label}",
+                        indexLabel: "{label} ({y})",
+                        yValueFormatString: "#,##0.#" % "",
+                        dataPoints: [{
+                                label: "Poder",
+                                y: `${datosApi.powerstats.power}`
+                            },
+                            {
+                                label: "Combate",
+                                y: `${datosApi.powerstats.combat}`
+                            },
+                            {
+                                label: "Durabilidad",
+                                y: `${datosApi.powerstats.durability}`
+                            },
+                            {
+                                label: "Rapidez",
+                                y: `${datosApi.powerstats.speed}`
+                            },
+                            {
+                                label: "Fuerza",
+                                y: `${datosApi.powerstats.strength}`
+                            },
+                            {
+                                label: "Inteligencia",
+                                y: `${datosApi.powerstats.intelligence}`
+                            },
+                        ]
                     }]
                 };
-                $("#chartContainer").CanvasJSChart(options); 
+                $("#chartContainer").CanvasJSChart(options);
             }
-
         });
     };
-}); 
+});
